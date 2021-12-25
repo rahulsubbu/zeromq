e4649279f4d8b4bcf38076df41379f1b6158f097
Subramanya <subramanya.musale@gmail.com>
2021-12-25 10:31:23 +0530

    Readme and zeromq req and rep

---
+++ README.txt
@@ -0,0 +1,5 @@
+Install zeromq library in linux machine
+#sudo apt-get install libzmq-dev
+
+compilation
+gcc zmq_req.c -o zmq_req -lzmq
---
+++ zeromq_req_rep/zero_rep.c
@@ -0,0 +1,30 @@
+/****************************************************************************
+*                                                                           *
+*	Author:  Subramanya N Musale                                        *
+*	Created: 25-12-2021                                                 *
+*	Description: Sample IPC mechanism to learn send and receive data    *
+*		     via zeromq                                             *
+*                                                                           *
+****************************************************************************/
+#include <zmq.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+
+int main (void)
+{
+	void *context = zmq_ctx_new ();
+	void *responder = zmq_socket (context, ZMQ_REP);
+	int rc = zmq_bind (responder, "tcp://*:5555");
+	assert (rc == 0);
+
+	while (1) {
+		char buffer [10];
+	        zmq_recv (responder, buffer, 10, 0);
+		printf ("Received Hello\n");
+		sleep (1);          //  Do some 'work'
+		zmq_send (responder, "World", 5, 0);
+	}
+	return 0;
+}
